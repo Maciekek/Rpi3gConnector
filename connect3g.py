ce8c0abbe314021fb948417b6705b290744379bd
maciekek <maciekek@gmail.com>
2015-02-23 21:50:41 +0000

    change ping method

--- connect3g.py
+++ connect3g.py
@@ -25,21 +25,25 @@ def getConnect():
 print "Hi"
 print "Ping run"
 
-cmdRun("sudo /home/pi/Rpi3gConnector/ping.sh")
-pingLog = open("/home/pi/Rpi3gConnector/ping.log", "r").read()
-
-connectStatus = re.search("1 received", pingLog);
-#print pingLog
-#print connectStatus.group()
+def checkConnection():
+	print "Connection check... "
+	pingResult = cmdRun("ping -c 1 wp.pl > ping.log") 
+	#pingLog = open("/home/pi/Rpi3gConnector/ping.log", "r").read()
+	print pingResult
+	connectStatus = re.search("1 received", pingResult);
+	#print pingLog
+	#print connectStatus.group()
+	return connectStatus
 
 while (True):
+	connectStatus = checkConnection()
 	if connectStatus:
 		print "Polaczenie aktywne"
 	else:
 		print "Brak polaczenia... Polaczenie zostanie wznowione"
 		getConnect()
 	
-	time.sleep(300)
+	time.sleep(20)
 
 		
 
